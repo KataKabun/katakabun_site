e03d1d22c45d3175234f1ce02d375a7bea2e44fb
Kata Kabun <katakabun@proton.me>
2023-07-23 05:37:52 -0400

    Initial commit.

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import browserslistToEsbuild from "browserslist-to-esbuild";
+
+export default defineConfig({
+	build: {
+		cssMinify: "lightningcss",
+		target: browserslistToEsbuild(["last 1 version and > 0.1%", "> 0.25%"]),
+	},
+	css: {
+		transformer: "lightningcss",
+		lightningcss: {
+			drafts: {
+				nesting: true,
+				customMedia: true,
+			},
+			sourceMap: true,
+		},
+	},
+	publicDir: "static",
+});
