53d25ef4152a13c31019ebf80a1e4af09e45cb7c
Kata Kabun <katakabun@proton.me>
2023-08-14 20:47:20 -0400

    chore: update .eleventy.config.cjs
    Signed-off-by: Kata Kabun <katakabun@proton.me>

--- .eleventy.config.cjs
+++ .eleventy.config.cjs
@@ -1,5 +1,5 @@
 const pluginWebc = require("@11ty/eleventy-plugin-webc");
-const lightningCSS = require("@11tyrocks/eleventy-plugin-lightningcss");
+// const lightningCSS = require("@11tyrocks/eleventy-plugin-lightningcss");
 
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPlugin(pluginWebc, {
@@ -9,9 +9,12 @@ module.exports = function (eleventyConfig) {
 		]
 	});
 
-	eleventyConfig.addPlugin(lightningCSS, {
-		minify: false
-	});
+	/*
+	 *	This plugin is a lot of trouble.
+	 *	eleventyConfig.addPlugin(lightningCSS, {
+	 *		minify: false
+	 *	});
+	*/
 
 	eleventyConfig.addPassthroughCopy("src/images");
 	eleventyConfig.addPassthroughCopy("src/fonts");
@@ -31,7 +34,8 @@ module.exports = function (eleventyConfig) {
 		markdownTemplateEngine: false,
 		dir: {
 			input: "src",
-			output: "dist"
+			output: "dist",
+			layouts: "_includes"
 		}
 	}
 };
