2e1259be0216bbbc7a92e57d51da539e0c6c0c8d
Kata Kabun <katakabun@proton.me>
2023-08-14 20:49:44 -0400

    feat: big update to layout hierarchy for site
    Signed-off-by: Kata Kabun <katakabun@proton.me>

---
+++ src/_data/metadata.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+	title: "Kata Kabun's Website",
+	author: "Kata Kabun",
+	description: "Information about the comfy bunny doll VTuber from Joy Kabuns who makes art, stories, music, and more.",
+	locale: "en_US",
+	base_url: "https://katakabun.neocities.org/",
+};
